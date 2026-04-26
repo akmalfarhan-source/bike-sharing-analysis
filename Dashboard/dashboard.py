2e0997d626cedf4fde89bece8a9a519d53054038
macbook <macbook@MacBook-Pro-M1.local>
2026-04-26 22:23:39 +0700

    fix file path

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
-df_day = pd.read_csv('main_data.csv')
+df_day = pd.read_csv(os.path.join(os.path.dirname(__file__), 'main_data.csv'))
 df_day['dteday'] = pd.to_datetime(df_day['dteday'])
 
-df_hour = pd.read_csv('../data/hour.csv')
+df_hour = pd.read_csv(os.path.join(os.path.dirname(__file__), '../data/hour.csv'))
 df_hour['dteday'] = pd.to_datetime(df_hour['dteday'])
 
 kolom = ['season', 'yr', 'mnth', 'holiday', 'weekday', 'workingday', 'weathersit']
